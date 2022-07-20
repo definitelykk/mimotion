7a330ecff03f9c85f213975e0872f17ffbb7d80c
definitelykk <109642853+definitelykk@users.noreply.github.com>
2022-07-20 18:14:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -265,7 +265,7 @@ def main(_user,_passwd,min_1, max_1):
     response = requests.post(url, data=data, headers=head).json()
     # print(response)
     result = f"[{now}]\n账号：{user[:3]}****{user[7:]}\n修改步数（{step}）[" + response['message'] + "]\n"
-    push_title = f"账号{user[7:]}\n修改步数（{step}）\n"
+    push_title = f"账号{user[7:]}修改步数（{step}）"
     #print(result)
     return result
 
